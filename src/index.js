df7c74e9b419371e68721e51453cf9563c784538
AlinurAIUR <yedilalinur@gmail.com>
2025-02-04 20:41:28 +0500

    step 8 Using arrays as components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import AppHeader from "./components/app-header";
 import SearchPanel from "./components/search-panel";
 import TodoList from "./components/todo-list";
@@ -8,11 +7,17 @@ import TodoList from "./components/todo-list";
 
 
 const App = () => {
+    const todoData = [
+        { label: 'Drink Coffee', important: false },
+        { label: 'Implement React Application', important: true },
+        { label: 'Make notes from your stud', important: false },
+        { label: 'Do not drink alchohol', important: false },
+    ];
     return (
         <div>
             <AppHeader />
             <SearchPanel />
-            <TodoList />
+            <TodoList todos =  {todoData} />
         </div>
     );
 };
