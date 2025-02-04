3d360dacded77e42ec01bd99843e299dc97aef4f
AlinurAIUR <yedilalinur@gmail.com>
2025-02-04 19:26:38 +0500

    step 6 Creating and formmating Project structure

--- src/index.js
+++ src/index.js
@@ -1,28 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const TodoList = () => {
-    const items = ['Install React', 'Study React', 'Use React', 'Build React App'];
-    return (
-        <ul>
-            {items.map((item, index) => (
-                <li key={index}>{item}</li>
-            ))}
-        </ul>
-    );
-};
+import AppHeader from "./components/app-header";
+import SearchPanel from "./components/search-panel";
+import TodoList from "./components/todo-list";
 
-const AppHeader = () => {
-    return <h1>My Todo List</h1>;
-};
 
-const SearchPanel = () => {
-    const searchText = 'Type here to search';
-    const searchStyle = {
-        fontSize: '20px'
-    };
-    return <input style={searchStyle} placeholder={searchText} disabled={true} />;
-};
 
 const App = () => {
     return (
