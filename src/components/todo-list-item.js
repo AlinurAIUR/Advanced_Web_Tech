780569331da4c488fbe240d5c18415025426d435
AlinurAIUR <yedilalinur@gmail.com>
2025-02-05 11:29:42 +0500

    step 13 Preparing for Todo List Project status filter

--- src/components/todo-list-item.js
+++ src/components/todo-list-item.js
@@ -1,11 +1,14 @@
 import React from "react";
 import './todo-list-item.css';
+
 const TodoListItem = ({ label, important = false }) => {
+
     const style = {
         color: important ? 'tomato' : 'black'
     };
     return <span className={ "todo-list-item" }
-                 style={ style }> { label }
+                 style={ style }>
+        { label }
     </span>;
 
 };
